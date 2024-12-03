6eb47eac7e68cdee7e0752e15251319e0dbfb86c
Ashesh Vidyut <ashesh.vidyut@hashicorp.com>
2024-12-03 17:32:30 +0530

    escaping

--- 6-leaderboard/terraform/userdata.sh
+++ 6-leaderboard/terraform/userdata.sh
@@ -22,7 +22,7 @@ server {
 
     # Proxy Node.js
     location /api/ {
-        rewrite ^/api(/.*)$ $1 break;
+        rewrite ^/api(/.*)$ \$1 break;
         proxy_pass http://127.0.0.1:3000;
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
