7b62ca6181bf070182d02f7554a1db4f74d00f47
Ashesh Vidyut <ashesh.vidyut@hashicorp.com>
2024-12-03 17:28:25 +0530

    fix redirection

--- 6-leaderboard/terraform/main.tf
+++ 6-leaderboard/terraform/main.tf
@@ -61,44 +61,7 @@ resource "aws_instance" "nginx_node_instance" {
   key_name      = aws_key_pair.leaderboard-key.key_name
   security_groups = [aws_security_group.nginx_node_sg.name]
 
-  user_data = <<-EOF
-    #!/bin/bash
-    # Update packages
-    apt-get update -y
-    apt-get upgrade -y
-
-    # Install Nginx and Node.js
-    apt-get install -y nginx
-    curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
-    apt-get install -y nodejs
-    mkdir -p /var/www/html
-
-    # Configure Nginx
-    cat > /etc/nginx/sites-available/default <<-EOL
-    server {
-        listen 80;
-
-        # Serve HTML
-        location / {
-            root /var/www/html;
-            index index.html;
-        }
-
-        # Proxy Node.js
-        location /api/ {
-            rewrite ^/api(/.*)$ '$1' break;
-            proxy_pass http://127.0.0.1:3000;
-            proxy_http_version 1.1;
-            proxy_set_header Upgrade \$http_upgrade;
-            proxy_set_header Connection 'upgrade';
-            proxy_set_header Host \$host;
-            proxy_cache_bypass \$http_upgrade;
-        }
-    }
-    EOL
-    systemctl restart nginx
-    npm install -g pm2
-  EOF
+  user_data = file("userdata.sh")
 
   provisioner "remote-exec" {
     inline = [
